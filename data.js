c6dc6ff83723e1493115695da23d721888047872
haye-han <okarina098@gmail.com>
2026-02-18 08:27:53 +0900

    미리안드 분리, 루실리카, 시프리에드 완료

--- data.js
+++ data.js
@@ -117,254 +117,70 @@ const HERO_DATA = [
                 label: "영웅 화면 10",
                 transcript: "가끔은 모든 것이 귀찮아서 그냥 잠에 빠져 버리고 싶을 때도 있습니다만. 글쎄요. 다치고, 아파하고, 고통스러워 하는 멍청한 자들을 두고 어떻게 그럴 수가 있겠어요?",
                 audio: "syphfride/light_syphfride/영웅화면10.mp3"
+            },
+                        {
+                id: "v_auto_1771367972",
+                label: "영웅 화면 11",
+                transcript: "드래곤은 주기적으로 수면기에 들어가요. 조금 경우가 다르지만 저도 예외가 아니죠. 수면기는 멀었으니 걱정하지 마시길. 오더라도 좀 참아보죠, 뭐.",
+                audio: "syphfride/light_syphfride/영웅화면11.mp3"
+            },
+            {
+                id: "v_auto_1771368007",
+                label: "영웅 화면 12",
+                transcript: "용인족, 흥미로운 아이들이랍니다. 동방의 용들에게 축복 받은 아이들이죠. 고향을 떠났더라도 말이에요.",
+                audio: "syphfride/light_syphfride/영웅화면12.mp3"
+            },
+            {
+                id: "v_auto_1771368052",
+                label: "영웅 화면 13",
+                transcript: "흠, 살면서 정말 여러 가지 독한 술들을 마셔봤지만, 취하지 못한다는 것은 아쉽네요. 술 취한 단명종이 얼마나 멍청한 짓을 할 수 있는지 아시나요? 한 번 쯤 그래보고 싶기도 한데.",
+                audio: "syphfride/light_syphfride/영웅화면13.mp3"
+            },
+            {
+                id: "v_auto_1771368095",
+                label: "영웅 화면 14",
+                transcript: "살면서 마셔본 가장 독한 술은, 북방 대륙의 거인들이 담은 술이었죠. 아, 물론 취하진 않았어요.",
+                audio: "syphfride/light_syphfride/영웅화면14.mp3"
+            },
+            {
+                id: "v_auto_1771368141",
+                label: "영웅 화면 15",
+                transcript: "헬가, 발터, 아슬란. 모두 2차 마도대전 때 최전방에서 싸우던 친구들이에요. 그들 몸에 흉터, 보셨죠? 치유에도 한계가 있는 법이죠.",
+                audio: "syphfride/light_syphfride/영웅화면15.mp3"
+            },
+            {
+                id: "v_auto_1771368204",
+                label: "영웅 화면 16",
+                transcript: "제가 좋아하는 음식이라면, 레드 와인을 곁들인 육즙이 뚝뚝 떨어지는 레어 스테이크. 하, 왜요? 저도 고기 좋아한다고요.",
+                audio: "syphfride/light_syphfride/영웅화면16.mp3"
+            },
+            {
+                id: "v_auto_1771368245",
+                label: "영웅 화면 17",
+                transcript: "해산물은 별로. 그래서, 사르디나는 잘 가지 않게 되더군요.",
+                audio: "syphfride/light_syphfride/영웅화면17.mp3"
+            },
+            {
+                id: "v_auto_1771368291",
+                label: "영웅 화면 18",
+                transcript: "검은… 산양? 아니, 이 자는 다르군.",
+                audio: "syphfride/light_syphfride/영웅화면18.mp3"
+            },
+            {
+                id: "v_auto_1771368325",
+                label: "영웅 화면 19",
+                transcript: "부상자가 생기면 불러 주시길. 그 외의 용무는 거절할게요.",
+                audio: "syphfride/light_syphfride/영웅화면19.mp3"
+            },
+            {
+                id: "v_auto_1771368362",
+                label: "영웅 화면 20",
+                transcript: "아니, 드래곤으로 안 변해요! 안 하는 게 아니라, 못 한다고요! 설마 기대했나요?",
+                audio: "syphfride/light_syphfride/영웅화면20.mp3"
             }
 
         ]
     },
-        {
-        id: "water_miriande",
-        name: "미리안드",
-        element: "water",
-        title: "근원에 닿은 이치",
-        image: "miriande/water_miriande.png",
-        voices: [
-             {
-                id: "v_auto_1771293330",
-                label: "자기소개",
-                transcript: "미리안드 레흐티넨, 마법사다.",
-                audio: "miriande/water_miriande/자기소개1.mp3"
-            },
-            {
-                id: "v_auto_1771293376",
-                label: "영웅 영입 1",
-                transcript: "네 부름이 들리기에 응했다.",
-                audio: "miriande/water_miriande/영웅영입1.mp3"
-            },
-            {
-                id: "v_auto_1771293406",
-                label: "영웅 영입 2",
-                transcript: "내 이름은 잊어버리지 않았나 보군.",
-                audio: "miriande/water_miriande/영웅영입2.mp3"
-            },
-                        {
-                id: "v_auto_1771298080",
-                label: "영웅 초월 1",
-                transcript: "기묘한 능력이군.",
-                audio: "miriande/water_miriande/영웅초월1.mp3"
-            },
-            {
-                id: "v_auto_1771298118",
-                label: "영웅 초월 2",
-                transcript: "네 힘, 마법과는 다른 듯한데.",
-                audio: "miriande/water_miriande/영웅초월2.mp3"
-            },
-            {
-                id: "v_auto_1771298144",
-                label: "영웅 초월 3",
-                transcript: "대가로는 무얼 원하지?",
-                audio: "miriande/water_miriande/영웅초월3.mp3"
-            },
-            {
-                id: "v_auto_1771298369",
-                label: "영웅 화면 1",
-                transcript: "이곳은 더워. 그래서 배정 받은 방에 냉기 마법을 걸었더니, 네 행정관이라는 자가 와서, 옆 방에도 영향이 가니 그만 두라고 했다. 나더러 어쩌라는 거지?",
-                audio: "miriande/water_miriande/영웅화면.mp3"
-            },
-            {
-                id: "v_auto_1771298238",
-                label: "영웅 화면 2",
-                transcript: "마법은, 유일하게 흥미를 둔 학문이었다. 배운 지 이틀 차에 선생이라는 자들을 넘어 섰었지. 일주일이 지나니, 아무도 내 말에 토를 달지 못했고.",
-                audio: "miriande/water_miriande/영웅화면 (1).mp3"
-            },
-                        {
-                id: "v_auto_1771298549",
-                label: "영웅 화면 3",
-                transcript: "너는 50년이 지나도 달라진 게 없군. 한결 같다는 건 때론 좋은 의미이기도 하지만, 여전히 그 거북이 같은 속도로만 뛸 수 있다면 문제가 있겠지.",
-                audio: "miriande/water_miriande/영웅화면 (2).mp3"
-            },
-            {
-                id: "v_auto_1771298709",
-                label: "영웅 화면 4",
-                transcript: "사회적 합의를 불필요한 것이라고 여기는 멍청이들이 있지. 모두가 밥 먹듯 범죄를 저지른다면 문명이 어떻게 성립하겠나? 발전은, 번영은? 선은 마음이라는 불확실한 것에서 나오는 게 아니다. 필요로부터 기인하는 거야.",
-                audio: "miriande/water_miriande/영웅화면 (3).mp3"
-            },
-            {
-                id: "v_auto_1771298770",
-                label: "영웅 화면 5",
-                transcript: "작은 꼬마, 내 말이 어렵나? 네 조그만 머리통을 위해 순화할 필요성이 있겠군. 알았다. 나인, 나인이라고. 기억해 두지.",
-                audio: "miriande/water_miriande/영웅화면 (4).mp3"
-            },
-                        {
-                id: "v_auto_1771303483",
-                label: "영웅 화면 6",
-                transcript: "하아, 너의 부하들은 시끄럽군. 이 성에서 내가 얼굴을 아는 건 너와 그 오랜 악연들 뿐이다. 자연히 소통을 위해서는 너를 찾을 수밖에 없는데, 왜 저들이 더 난리를 피우는 거지?",
-                audio: "miriande/water_miriande/영웅화면 (5).mp3"
-            },
-            {
-                id: "v_auto_1771303550",
-                label: "영웅 화면 7",
-                transcript: "아카데미, 그런 기관이 있다는 이야기는 들었다. 마땅한 일이지. 마력을 보유한 자들의 수는 늘어나는데, 양성 기관이 없어서야 비효율적이지 않겠나.",
-                audio: "miriande/water_miriande/영웅화면 (6).mp3"
-            },
-            {
-                id: "v_auto_1771303623",
-                label: "영웅 화면 8",
-                transcript: "마법과 초상 능력은 달라. 초상 능력은 고유의 자질이니, 노력한다 해서 새로 익힐 수 없지. 개개인이 가진 흐름은 사람의 수만큼이나 천차만별이니, 응당 비슷한 형태의 마법이 존재할 수밖에. 원리는 전혀 다르지만 말이야.",
-                audio: "miriande/water_miriande/영웅화면 (7).mp3"
-            },
-            {
-                id: "v_auto_1771303713",
-                label: "영웅 화면 9",
-                transcript: "글쎄, 내가 움직였던 건 가능하리라는 판단이 섰기 때문이다. 아직 못 본 경치가 셀 수 없이 많은데, 사사로이 목숨을 포기할 수는 없었지. 덕분에 바보들이 모이면 그럭저럭 나쁘지 않은 결과가 나온다는 사실도…. 알았고.",
-                audio: "miriande/water_miriande/영웅화면 (8).mp3"
-            },
-            {
-                id: "v_auto_1771303817",
-                label: "영웅 화면 10",
-                transcript: "오늘은 바람이 예사롭지 않군. 범인들은 모두 문을 걸어 잠그고 나오지 않는 편이 좋을 거다. 음? 걱정이라니. 이는 효율의 문제다. 아직도 모르겠나?",
-                audio: "miriande/water_miriande/영웅화면 (9).mp3"
-            },
-                        {
-                id: "v_auto_1771308752",
-                label: "영웅화면 11",
-                transcript: "처음 세상에 나왔을 때는 온갖 것들이 궁금했다. 계절은 왜 시시각각 변하는지, 인간 군상이란 이렇게도 다양한지. 오래 담아두지 못하니 최대한 많은 걸 마주하려고 했지. 그래. 너희들을 만나기 전까지는.",
-                audio: "miriande/water_miriande/영웅화면 (10).mp3"
-            },
-            {
-                id: "v_auto_1771308808",
-                label: "영웅화면 12",
-                transcript: "적절한 주문은 마법의 위력을 증폭시킨다. 별개로 적절하지 못한 주문은 위력을 반감시키지. 말에는 힘이 있다는 이야기를 들어보지 못했나? 마법이든, 물리적인 공격이든 이 원리는 마찬가지야.",
-                audio: "miriande/water_miriande/영웅화면 (11).mp3"
-            },
-            {
-                id: "v_auto_1771308907",
-                label: "영웅화면 13",
-                transcript: "대화를 나누고, 안색을 살피고. 상대의 지적 수준을 고려해야 하지. 소통이란 이렇게 불편함을 감수하고 이루어지는 일이다. 열 두 명으로 족해. 그러니 나를 더 귀찮게 하지 마라.",
-                audio: "miriande/water_miriande/영웅화면 (12).mp3"
-            },
-            {
-                id: "v_auto_1771308971",
-                label: "영웅화면 14",
-                transcript: "50년 동안 내 얼굴이 달라지지 않은 게 신기한가? 세상은 방대하고 익힐 것은 많지. 그에 비해 인간의 수명은 짧으니 방법을 찾아야 했어.",
-                audio: "miriande/water_miriande/영웅화면 (13).mp3"
-            },
-            {
-                id: "v_auto_1771309011",
-                label: "영웅화면 15",
-                transcript: "천체의 운행은 우리의 힘에도 영향을 준다. 오늘 별들의 정렬은 나쁘지 않군.",
-                audio: "miriande/water_miriande/영웅화면 (14).mp3"
-            },
-            {
-                id: "v_auto_1771309105",
-                label: "영웅화면 16",
-                transcript: "북방대륙의 기후? 척박하고, 건조하고, 춥고 황폐하지. 얼음 궁전에 대해 들어본 적 있는지 모르겠군. 아, 대륙 전체가 문레이크와 비슷하다고 보면 되겠지. 지긋지긋한 것까지 닮았어.",
-                audio: "miriande/water_miriande/영웅화면 (15).mp3"
-            },
-            {
-                id: "v_auto_1771309185",
-                label: "영웅화면 17",
-                transcript: "그 엄동설한에 구태여 가려 드는 이유를 모르겠군. 내가 나고 자란 곳을 보고 싶다고? 흥! 발을 내리자마자 벌벌 떨지나 않으면 다행일 텐데.",
-                audio: "miriande/water_miriande/영웅화면 (16).mp3"
-            },
-            {
-                id: "v_auto_1771309249",
-                label: "영웅화면 18",
-                transcript: "며칠 뜬 눈으로 지새우는 건 아무렇지도 않다 말했을 텐데. 쓸데없는 데 시간을 낭비하지 마라. 너는 군주 된 자가 아닌가.",
-                audio: "miriande/water_miriande/영웅화면 (17).mp3"
-            },
-            {
-                id: "v_auto_1771309322",
-                label: "영웅화면 19",
-                transcript: "쉿!  완성하기 일보 직전이다. 5분? 아니, 10분만 있다 와. …가라고 했지 않나!",
-                audio: "miriande/water_miriande/영웅화면 (18).mp3"
-            },
-            {
-                id: "v_auto_1771309403",
-                label: "영웅화면 20",
-                transcript: "시프리에드. 너에게 적당히, 라는 말이 통하지 않는다는 걸 안다. 하지만 내 실험용 냉기 결정들을 알코올 덩어리에 곁들이는 얼음 정도로 취급하지는 말아줬으면 좋겠는데.",
-                audio: "miriande/water_miriande/영웅화면 (19).mp3"
-            },
-            {
-                id: "v_auto_1771309510",
-                label: "영웅 화면 21",
-                transcript: "정에 물들기 쉬운 이만큼 비효율적인 부류는 없지. 이를테면 능구렁이 같은 척, 제 이익만 계산하는 척하면서 끝끝내 사람을 포기하지 못하는 놈 말이다. 너도 한 명 알고 있을 텐데.",
-                audio: "miriande/water_miriande/영웅화면 (20).mp3"
-            },
-            {
-                id: "v_auto_1771309595",
-                label: "영웅 화면 22",
-                transcript: "음. 불침번을 설 때는 알카나스와 한 조일 때가 가장 편안했지. 쓸데없는 질문이나 사담에 반응할 필요가 없으니, 할 일에만 집중할 수 있었다.",
-                audio: "miriande/water_miriande/영웅화면 (21).mp3"
-            },
-            {
-                id: "v_auto_1771309649",
-                label: "영웅 화면 23",
-                transcript: "아슬란이 왕이 되었다는 소식은 알고 있었다. 그는 모르겠지만, 일이 끝나고 난 뒤에도 델포이아에 몇 번 방문한 적이 있거든. 아, 이제는 다케온이라고 불러야 하나?",
-                audio: "miriande/water_miriande/영웅화면 (22).mp3"
-            },
-            {
-                id: "v_auto_1771309775",
-                label: "영웅 화면 24",
-                transcript: "크메르사트와 헬가가 나눈 영혼 계약은 마법의 일종이다. 정도 이상의 마력을 다룰 수 있고, 희생을 감수할 수 있다면 누구든 시도할 수 있지. 득보다 실이 크기에…. 굳이 행동에 옮기지 않는 편을 추천하겠다만.",
-                audio: "miriande/water_miriande/영웅화면 (23).mp3"
-            },
-            {
-                id: "v_auto_1771309861",
-                label: "영웅 화면 25",
-                transcript: "음? 발터에게 건넸던 조언들이 과하다고는 생각하지 않아. 제 아무리 그의 의지가 아니었다 한들, 일어난 일을 되돌릴 수 있나? 멍청하게 주저 앉아 있는 것보단, 주어진 힘을 어떻게 활용할지 생각해야지.",
-                audio: "miriande/water_miriande/영웅화면 (24).mp3"
-            },
-            {
-                id: "v_auto_1771309942",
-                label: "영웅 화면 26",
-                transcript: "북방 대륙에는 드라이어드가 없어. 자생할 수 없는 환경이다. 그래서인지 종종 이카테스톨이 북방에 관해 질문하고는 했는데…. 제대로 대답해 준 적은 없군. 알아봤자 좋을 것도 없고.",
-                audio: "miriande/water_miriande/영웅화면 (25).mp3"
-            },
-            {
-                id: "v_auto_1771310019",
-                label: "영웅 화면 27",
-                transcript: "시끄러운 건 딱 질색이다. 원치 않는 소음이라면 더더욱. 라우젤릭의 노래 소리는 훌륭했지만, 잠을 깨우는 목청은 달갑지 않았어. 말이 통하지 않으니 차단 마법을 걸어둘 때도 많았지.",
-                audio: "miriande/water_miriande/영웅화면 (26).mp3"
-            },
-            {
-                id: "v_auto_1771310212",
-                label: "영웅 화면 28",
-                transcript: "아힐람에게 깃든 태양의 힘은 무어라 설명할 수 없어. 마법도 ,공학도, 어느 분류에도 속하지 않는다. 저 멀리 상고시대로부터 전해진 원초적 의지의 화신이니.",
-                audio: "miriande/water_miriande/영웅화면 (27).mp3"
-            },
-            {
-                id: "v_auto_1771310260",
-                label: "영웅 화면 29",
-                transcript: "라르곤 같은 이가 우리를 만날 때까지 살아남았다는 점이 세상의 자비로운 면이겠지. 물론 세상은 그에게 구원 받았으니, 걸맞는 대가를 치른 셈이 되겠군.",
-                audio: "miriande/water_miriande/영웅화면 (28).mp3"
-            },
-            {
-                id: "v_auto_1771310408",
-                label: "영웅 화면 30",
-                transcript: "브랜든의 마력은 특수한 파장을 가지고 있다. 그 육신이 더없이 특별한 상태이기 때문이겠지. 저주를 파훼할 수 있는 방법도 연구해 보았지만 모조리 실패하더군. 세상이 잿더미로 화해도 그의 육신은 살아남을까? 그럼 어떤 기분일런지…. 흥미롭지 않은가?",
-                audio: "miriande/water_miriande/영웅화면 (29).mp3"
-            }
-
-        ]
-    },
-//        {
-//        id: "earth_miriande",
-//        name: "미리안드",
-//        element: "earth",
-//        title: "백록의 마법사",
-//        image: "miriande/earth_miriande.png",
-//        voices: [
-//        ]
-//    },
-//        {
-//        id: "fire_miriande",
-//        name: "미리안드",
-//        element: "fire",
-//        title: "순리의 역행자",
-//        image: "miriande/fire_miriande.png",
-//        voices: [
-//        ]
-//    },
 //        {
 //        id: "earth_raligon",
 //        name: "라르곤",
@@ -1001,6 +817,78 @@ const HERO_DATA = [
                 label: "영웅 화면 17",
                 transcript: "추천하는 과실주요? 플로렌스산 시드르가 최고였죠! 캬라멜과 같이 마셔도 맛있고, 단품으로도 훌륭하니까요! 아…. 그대에게는… 조금 독할지도?",
                 audio: "lucilica/lucil (24).mp3"
+            },
+                        {
+                id: "v_auto_1771369769",
+                label: "영웅 화면 18",
+                transcript: "성인이나 현자들에게도 약점은 있는 법이에요. 자기 과시는 독이나 다름 없지요. 언제나 경계하고 주의해야만 발전할 수 있을 테니까요.",
+                audio: "lucilica/lucil (25).mp3"
+            },
+            {
+                id: "v_auto_1771369816",
+                label: "영웅 화면 19",
+                transcript: "가끔…. 다닐이 그리울 때가 있어요. 물론 이곳의 행정도 훌륭하지만 그만큼 유능한 비서관은 쉽게 만날 수 있는 인연이 아니니까요.",
+                audio: "lucilica/lucil (26).mp3"
+            },
+            {
+                id: "v_auto_1771369847",
+                label: "영웅 화면 20",
+                transcript: "요즘 루미에 경에게 스케이트를 배우고 있어요. 본 강의보다는 사담이 몇 배나 많긴 해도 유익한 시간이랍니다. 엘펜하임 출신이 빙판에 약해서는 안 되니까요.",
+                audio: "lucilica/lucil (27).mp3"
+            },
+            {
+                id: "v_auto_1771369933",
+                label: "영웅 화면 21",
+                transcript: "바네사 경이 연주회에 초대해 주셔서 다녀왔어요. 음악은 훌륭한 취미죠. 스트레스 해소에도 도움을 주고요. 저도 어릴 적에 피아노를 시도해 본 적이 있는데, 실수로 건반을 부숴 먹는 바람에. …아차, 이 뒤는 비밀이에요.",
+                audio: "lucilica/lucil (28).mp3"
+            },
+            {
+                id: "v_auto_1771370032",
+                label: "영웅 화면 22",
+                transcript: "저는 우울한 때가 없냐고요? 그럴 리가요. 짜증도 나고, 우울할 때도 있고, 화도 나는 법이지요. 그렇지만 한 순간의 감정에 매몰되어선 앞으로 나아갈 수 없어요. 기분이 태도가 돼서는 안 되니까요.",
+                audio: "lucilica/lucil (29).mp3"
+            },
+            {
+                id: "v_auto_1771370075",
+                label: "영웅 화면 23",
+                transcript: "바레타 경과 대련하는 건 즐거워요. 형태가 비슷한 무기를 사용하지만 방식은 전혀 다르거든요. 가끔 라샤드가 나타나서 방해할 때가 있다는 게 문제이긴 한데.",
+                audio: "lucilica/lucil (30).mp3"
+            },
+            {
+                id: "v_auto_1771370176",
+                label: "영웅 화면 24",
+                transcript: "이거, 말인가요? 발터 경에게 드릴 보답이에요. 얼마 전에 품질이 좋은 원두를 선물 받았거든요. 우리 비서관이 가끔 타 오던 커피도 못지 않게 훌륭했던 기억이 나서, 맛보여 드리려고 오랜만에 연락해 물건을 공수했죠. 네? 더 이상…. 선물이 아닌 것 같다고요?",
+                audio: "lucilica/lucil (31).mp3"
+            },
+            {
+                id: "v_auto_1771370246",
+                label: "영웅 화면 25",
+                transcript: "어라, 아우레아 님. 팔은 왜 잡으시는 거죠? 네? 모델이요? 아, 잠깐…! 저 이제야 파견해서 돌아온 참인데!",
+                audio: "lucilica/lucil (32).mp3"
+            },
+            {
+                id: "v_auto_1771370357",
+                label: "영웅 화면 26",
+                transcript: "그때…. 두렵지 않았냐고요? 그럴 리가 있나요? 당연히 두려웠죠. 라플라스는 갑자기 이상한 소리나 해 대질 않나, 운신은 제한되질 않나. 그래도 아득바득 버텨낸 결과가 지금이라면, 그리 나쁜 결말은 아니로군요. 어머, 감동했나요?",
+                audio: "lucilica/lucil (33).mp3"
+            },
+            {
+                id: "v_auto_1771370438",
+                label: "영웅 화면 27",
+                transcript: "그대 주변에는 이상하게 인재가 많아요. 상등품의 장비를 아무렇지도 않게 술술 만들어 오는 린 양도 그렇고 잠은 자는 건지 의문인 루인 경도 그렇고. 어, 저도 포함된다고요? 어머. 호호! 어머, 이제 그런 말도 할 줄 알고?",
+                audio: "lucilica/lucil (34).mp3"
+            },
+            {
+                id: "v_auto_1771370514",
+                label: "영웅 화면 28",
+                transcript: "변화가 두려울 때도 있지요. 저도 사람인데다, 나름 실패도 겪어 보았거든요. 다만, 멈춰 서서는 의미가 없다는 걸 남들보다 빨리 깨달은 것 뿐이에요.",
+                audio: "lucilica/lucil (35).mp3"
+            },
+            {
+                id: "v_auto_1771370538",
+                label: "영웅 화면 29",
+                transcript: "식사는 했나요? 아직이라면 함께 가시죠.",
+                audio: "lucilica/lucil (36).mp3"
             }
         ]    
     },
