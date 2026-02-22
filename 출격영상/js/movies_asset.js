10d0c7ad0455e84825ad6dcb4fe797ecd4dccc51
yellowbricks <135861147+yellowbrickssss@users.noreply.github.com>
2026-02-23 05:22:58 +0900

    타이틀 수정
    하........... 너무 멀리 돌아왔다

--- 출격영상/js/movies_asset.js
+++ 출격영상/js/movies_asset.js
@@ -1,37 +1,37 @@
 
 const playlistData = [
     {
-        title: "Light - Ruin",
+        title: "[빛] - 루인",
         artist: "LOH",
         video: "asset/01 light_ruin.mp4",
         cover: "covers/light_ruin.png" 
     },
     {
-        title: "Light - Myu",
+        title: "[빛] - 뮤",
         artist: "LOH",
         video: "asset/02 light_myu.mp4",
         cover: "covers/light_myu.png"
     },
     {
-        title: "Earth - Johan",
+        title: "[대지] - 요한",
         artist: "LOH",
         video: "asset/03 earth_johan.mp4",
         cover: "covers/earth_johan.png"
     },
     {
-        title: "Water - Miriande",
+        title: "[물] - 미리안드",
         artist: "LOH",
         video: "asset/04 water_miriande.mp4",
         cover: "covers/water_miriande.png"
     },
     {
-        title: "Water - Rosanna",
+        title: "[물] - 로잔나",
         artist: "LOH",
         video: "asset/05 water_rosanna.mp4",
         cover: "covers/water_rosanna.png"
     },
-        {
-        title: "Earth - Ondal",
+    {
+        title: "[대지] - 온달",
         artist: "LOH",
         video: "asset/06 earth_ondal.mp4",
         cover: "covers/earth_ondal.png"
